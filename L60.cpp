df6bf3a08bc68a007f26851d65b3f73d1ca9e192
JosueMonterrey <josuemr0206@gmail.com>
2025-11-11 20:05:25 -0600

    Terminar L60.cpp

--- L60.cpp
+++ L60.cpp
@@ -1,7 +1,56 @@
 #include <bits/stdc++.h>
 
+#define vi vector<int>
+#define vvi vector<vi>
+#define lli long long int
+#define INF 1e18
+
 using namespace std;
 
 int main() {
+    int N;
+    cin >> N;
 
+    vi x(N), y(N);
+
+    int xi, yi;
+    for (int i = 0; i < N; i++) {
+        cin >> xi >> yi;
+
+        x[i] = xi;
+        y[i] = yi;
+    }
+
+    vi minDist(N, INF);
+    vector<bool> used(N, false);
+
+    minDist[0] = 0;
+    lli total = 0;
+
+    for (int i = 0; i < N; i++) {
+        int u = -1;
+        lli best = INF;
+
+        // Buscar el nodo no usado con menor distancia al árbol
+        for (int j = 0; j < N; ++j) {
+            if (!used[j] && minDist[j] < best) {
+                best = minDist[j];
+                u = j;
+            }
+        }
+
+        used[u] = true;
+        total += best;
+
+        // Actualizar distancias a los nodos no usados
+        for (int v = 0; v < N; ++v) {
+            if (!used[v]) {
+                lli d = llabs(x[u] - x[v]) + llabs(y[u] - y[v]);
+                if (d < minDist[v])
+                    minDist[v] = d;
+            }
+        }
+    }
+
+    cout << total << "\n";
 }
